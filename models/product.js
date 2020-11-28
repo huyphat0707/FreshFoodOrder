e55aab2b51f523e45c725d5e0bbbc0812fea517d
Vinh con <nguyenthanhvinhql@gmail.com>
2020-11-28 10:00:47 +0700

    add product and code dish

--- models/product.js
+++ models/product.js
@@ -2,46 +2,47 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Product = new Schema({
-  image: {type: String},
-  cateId: {type: Number, ref: 'cate'},
-  name: {type: String},
-  price: {type: Number},
-  typeBuy: {type: String},
-  description: {type: String},
-  storage: {type: String},
-  origin: {type: String},
-  usage: {type: String},
-  viewCounts: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  comment: {
-    total: {
-      type: Number,
-      require: false,
-      default: 0,
-    },
-    items: [
-      {
-        title: {
-          type: String,
-        },
-        content: {
-          type: String,
-        },
-        name: {
-          type: String,
-        },
-        date: {
-          type: Date,
-          default: Date.now,
-        },
-        star: {
-          type: Number,
-        },
-      },
-    ],
-  },
+	image: { type: String },
+	cateId: { type: Number, ref: 'cate' },
+	name: { type: String },
+	price: { type: Number },
+	typeBuy: { type: String },
+	description: { type: String },
+	storage: { type: String },
+	origin: { type: String },
+	usage: { type: String },
+	nutritional: { type: Object},
+	viewCounts: {
+		type: Number,
+		required: false,
+		default: 0,
+	},
+	comment: {
+		total: {
+			type: Number,
+			require: false,
+			default: 0,
+		},
+		items: [
+			{
+				title: {
+					type: String,
+				},
+				content: {
+					type: String,
+				},
+				name: {
+					type: String,
+				},
+				date: {
+					type: Date,
+					default: Date.now,
+				},
+				star: {
+					type: Number,
+				},
+			},
+		],
+	},
 });
 module.exports = mongoose.model('product', Product);
